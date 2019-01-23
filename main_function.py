9d613bbbdeb0d2e03d69d741ce50d4b3245d2ad5
smchakradeo <kshubhamchakradeo@gmail.com>
2019-01-23 13:10:48 -1000

    with kalman filter

--- main_function.py
+++ main_function.py
@@ -15,7 +15,12 @@ class main_Class(object):
         self.angle1 = 0.0
         self.angle2 = 0.0
         self.angle3 = 0.0
-        self.calib_result = np.array([0,0,0]).transpose()
+        self.A = np.identity(6)
+        self.B = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]],float)
+        self.Pk = np.identity(6)
+        self.Q = np.eye(6)
+        self.R = np.eye(3)
+        self.calib_result = np.array([0,0,0],float).transpose()
         self.ini_ori = np.identity(3)
         self.x_states = np.array([0, 0, 0, 0, 0, 0], float).transpose()
         self.x_states_1 = self.x_states
@@ -41,6 +46,7 @@ class main_Class(object):
             self.ini_ori[:,0] = mag/0.45
             self.ini_ori[:, 2] = grav/9.86
             self.ini_ori[:, 1] = np.cross(grav,mag)
+            print('or: ', self.ini_ori)
             self.ini_ori = np.linalg.inv(self.ini_ori)
             # Update the state vector and control vector here
             print('Initial: ', self.ini_ori)
@@ -100,6 +106,7 @@ class main_Class(object):
         self.ini_ori[2][0] = -y * sa + (1.0 - ca) * x * z
         self.ini_ori[2][1] = x * sa + (1.0 - ca) * y * z
         self.ini_ori[2][2] = 1.0 + (1.0 - ca) * (z ** 2 - 1.0)
+
     def calibration(self):
         # Send data
         sent = self.sock.sendto(bytes(self.message.encode()), self.server_address)
@@ -112,18 +119,26 @@ class main_Class(object):
             magr = np.array([float(data[9]), float(data[10]), float(data[11])]).transpose()
             self.calib_result += magr
             time.sleep(0.1)
+    def filesave(self,data_save):
+        #data_save = data_save.strip()
+        #data_save = str(data_save).strip('[ ]')
+        #data_save = data_save.strip("' '")
+        inp1 = open("Data1.txt", "a+")
+        inp1.write(data_save)
+        inp1.write("\n")
+        inp1.close()
 
 
     def motion_model(self, U, sensr):
         T = sensr.DT
-        A = np.array([[1, 0, 0, T, 0, 0],
+        self.A = np.array([[1, 0, 0, T, 0, 0],
                       [0, 1, 0, 0, T, 0],
                       [0, 0, 1, 0, 0, T],
                       [0, 0, 0, 1, 0, 0],
                       [0, 0, 0, 0, 1, 0],
                       [0, 0, 0, 0, 0, 1]], float)
 
-        B = np.array([[((T ** 2) / 2), 0, 0],
+        self.B = np.array([[((T ** 2) / 2), 0, 0],
                       [0, ((T ** 2) / 2), 0],
                       [0, 0, ((T ** 2) / 2)],
                       [T, 0, 0],
@@ -141,10 +156,48 @@ class main_Class(object):
         self.U = U
         orientation = sensr.Orientation
         orientation1 = sensr.Orientation_1
-        self.x_states = np.matmul(A, np.matmul(rotation, self.x_states)) + np.matmul(B, np.matmul(orientation, U_vec))
-        self.x_states_1 = np.matmul(A, np.matmul(rotation, self.x_states_1)) + np.matmul(B, np.matmul(orientation1, U_vec))
+        self.x_states = np.matmul(self.A, np.matmul(rotation, self.x_states)) + np.matmul(self.B, np.matmul(orientation, U_vec))
+        self.x_states_1 = np.matmul(self.A, np.matmul(rotation, self.x_states_1)) + np.matmul(self.B, np.matmul(orientation1, U_vec))
         print('states: ', self.x_states)
         print('states2: ', self.x_states_1)
+        return self.x_states
+
+
+    
+    def kalman_filter(self,zk):
+        """Performs Kalman Filtering on pandas timeseries data.
+        :param: zk (pandas timeseries): input data
+        :param: xk (np.array): a priori state estimate vector
+        :param: A (np.matrix): state transition coefficient matrix
+        :param: B (np.matrix): control coefficient matrix
+        :param: Pk (np.matrix): prediction covariance matrix
+        :param: uk (np.array): control signal vector
+        :param: wk (float): process noise (has covariance Q)
+        :param: Q (float): process covariance
+        :param: R (float): measurement covariance
+        :param: H (np.matrix):  transformation matrix
+        :return: output (np.array): kalman filtered data
+        """
+        try:
+            output = np.zeros(len(zk))
+            H = np.array([[1,0,0,0,0,0],[0,1,0,0,0,0],[0,0,1,0,0,0]])
+        
+            # time update (prediction)
+            #self.x_states = motion_model()
+            self.x_states = np.matmul(self.A,self.x_states) + np.matmul(self.B,self.U) #Predict state
+            zk_pred = np.matmul(H,self.x_states) # Predict measurement
+            self.Pk = np.matmul(self.A,np.matmul(self.Pk,self.A.T)) + self.Q # Predict error covariance
+            # measurement update (correction)
+            vk = zk - zk_pred # Measurement residual
+            S = (np.matmul(H,np.matmul(self.Pk,H.T))) + self.R # Prediction covariance
+            Kk = np.matmul(np.matmul(self.Pk,H.transpose()), np.linalg.inv(S)) # Compute Kalman Gain
+            self.x_states = self.x_states + np.matmul(Kk , vk) # Update estimate with gain * residual
+            self.Pk = np.matmul((1 - np.matmul(Kk,H)),self.Pk) # Update error covariance
+            output = self.x_states
+            print('Output: ', output)
+            return output
+        except np.linalg.linalg.LinAlgError:
+            pass
 
     def main(self):
         sensr = sensor_fusion(self.angle1, self.angle2, self.angle3, self.ini_ori, time.time())
@@ -163,8 +216,11 @@ class main_Class(object):
                     sensr.set_angles(alpha=float(data[6]), phi=float(data[7]), theta=float(data[8]),acc= accn,mag=magr,time_T=time.time())
                     U_vec = np.subtract(np.array([float(data[3]), float(data[4]), float(data[5])], float).transpose(),
                                         sensr.gravity)
-                    self.motion_model(U_vec, sensr)
-                    time.sleep(0.1)
+                    xk = self.motion_model(U_vec, sensr)
+                    zk = np.array([0,0,0]).T
+                    final_xyz = self.kalman_filter(zk)
+                    self.filesave(str(final_xyz))
+                    #time.sleep(0.1)
             finally:
                 pass
 
@@ -173,4 +229,5 @@ obj = main_Class()
 ini_time = time.time()
 while(time.time()-ini_time<=5):
     obj.calibration()
+print('Callibration Done')
 obj.main()
