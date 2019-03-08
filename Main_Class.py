d87d120886e0dc62fb0a986bdf8961c7ebb05044
shubham chakradeo <kshubhamchakradeo@gmail.com>
2019-03-07 16:08:28 -1000

    Working initial 3d sensor fusion

--- Main_Class.py
+++ Main_Class.py
@@ -36,11 +36,11 @@ class sensor_fusion(object):
         self.gravity = np.array([0,0,9.8]).transpose()
 
 
-    def set_angles(self,alpha,phi,theta,acc,mag,time_T):
-        time = time_T - self.time_T
+    def set_angles(self,acc,mag,time_T):
+        """time = time_T - self.time_T
         self.DT = time/1000
         self.time_T = time_T
-        """self.roll = self.DT*((alpha+self.alpha_dot)/2)
+        self.roll = self.DT*((alpha+self.alpha_dot)/2)
         self.pitch = self.DT * ((phi + self.phi_dot) / 2)
         self.yaw = self.DT * ((theta + self.theta_dot) / 2)
         self.alpha_dot = alpha
@@ -48,11 +48,11 @@ class sensor_fusion(object):
         self.theta_dot = theta"""
         
         #-----------------------------------
-        S = Quaternion(scalar=0.0,vector=[alpha,phi,theta])
+        """S = Quaternion(scalar=0.0,vector=[alpha,phi,theta])
         qdot =(0.5 * (self.quat * S))
         quat = self.quat + (qdot * self.DT)
         q_gy = quat.normalised
-        self.quat_gy = q_gy
+        self.quat_gy = q_gy"""
         """
         # ----------------------------------
         self.R_U[1][1] = math.cos(self.roll)
@@ -75,8 +75,10 @@ class sensor_fusion(object):
         self.gravity =  np.matmul(np.linalg.inv(self.Orientation),np.array([0,0,9.8]).transpose())
         """
         #------------------------------------
+        self.DT = time_T
+        acc = np.array([acc[0],acc[1],acc[2]]).transpose()
         self.Orientation_acc[:,2] = acc
-        self.Orientation_acc[:,1] = np.cross(acc,mag)
+        self.Orientation_acc[:,1] = np.cross(acc,np.array([mag[0],mag[1],mag[2]]).transpose())
         self.Orientation_acc[:,0] = np.cross(self.Orientation_acc[:,1], acc)
         self.Orientation_acc[:,0] = self.Orientation_acc[:,0]/np.linalg.norm(self.Orientation_acc[:,0])
         self.Orientation_acc[:, 1] = self.Orientation_acc[:, 1] / np.linalg.norm(self.Orientation_acc[:, 1])
@@ -86,21 +88,21 @@ class sensor_fusion(object):
         #--------------------------------------
         
         
-        yaw_a =  (math.atan2(2.0 * (q[1] *q[2] - q[0] * q[3]),
+        self.yaw_a =  (math.atan2(2.0 * (q[1] *q[2] - q[0] * q[3]),
                                                         -1+2*(q[0] * q[0] + q[1] * q[1])))
         pitch_a = (-math.asin(2.0 * (q[1] * q[3] + q[0] * q[2])))
         roll_a = (math.atan2(2.0 * (-q[0] * q[1] + q[2] * q[3]),
                                   -1+2*(q[0] * q[0] + q[1] * q[1])))
 		#-----------------------------------------
-        yaw_g =  (math.atan2(2.0 * (q_gy[1] *q_gy[2] - q_gy[0] * q_gy[3]),
+        """yaw_g =  (math.atan2(2.0 * (q_gy[1] *q_gy[2] - q_gy[0] * q_gy[3]),
                                                         -1+2*(q_gy[0] * q_gy[0] + q_gy[1] * q_gy[1])))
         pitch_g = (-math.asin(2.0 * (q_gy[1] * q_gy[3] + q_gy[0] * q_gy[2])))
         roll_g = (math.atan2(2.0 * (-q_gy[0] * q_gy[1] + q_gy[2] * q_gy[3]),
                                   -1+2*(q_gy[0] * q_gy[0] + q_gy[1] * q_gy[1])))
         #print(math.degrees(roll_a),math.degrees(pitch_a),math.degrees(yaw_a),math.degrees(roll_g),math.degrees(pitch_g),math.degrees(yaw_g))
         #-----------------------------------------
-        q_final = 0.8*self.q+(1-0.8)*self.quat_gy
-        self.q_final = q_final
+        #q_final = 0.8*self.q+(1-0.8)*self.quat_gy
+        #self.q_final = q_final
         #-----------------------------------------
         
         yaw =  (math.atan2(2.0 * (q_final[1] *q_final[2] - q_final[0] * q_final[3]),
@@ -109,7 +111,7 @@ class sensor_fusion(object):
         roll = (math.atan2(2.0 * (-q_final[0] * q_final[1] + q_final[2] * q_final[3]),
                                   -1+2*(q_final[0] * q_final[0] + q_final[1] * q_final[1])))
         #print(math.degrees(roll),math.degrees(pitch),math.degrees(yaw))
-        plt.pause(0.001)
+        plt.pause(0.001)"""
         
         
         
--- main_function.py
+++ main_function.py
@@ -7,42 +7,32 @@ import time
 from scipy import signal
 from pyquaternion import Quaternion
 
-#from Main_Class import sensor_fusion
-
-
-
+from Main_Class import sensor_fusion
 class main_Class(object):
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_address = ('127.0.0.1', 5678)
         self.message = str(random.randint(1, 101)) + ' 51'
-        self.angle1 = 0.0
-        self.angle2 = 0.0
-        self.angle3 = 0.0
         self.Phi= np.identity(15,float)
         self.B = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]],float)
-        #self.Pk = np.identity(6)
-        #self.Q = np.diag([1.0,1.0,1.0,1.0,1.0,1.0])
-        #self.R = np.diag([0.1,0.1,0.1])
         self.H = np.zeros((7,9),float)
         self.Pk = np.diag([0, 0, 0, 0.1, 0.1, 0.1, 0, 0, 0, 0, 0, 0, 0.1, 0.1, 0.1])
         self.Rk = np.diag([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
-        self.Qk = np.diag([0.0001, 0.0001, 0.0001, np.zeros((1, 6), float), 0.0001, 0.0001, 0.0001, np.zeros((1, 3), float)])
-        self.calib_result = np.array([0,0,0],float).transpose()
+        arr = np.concatenate((np.array([[0.0001, 0.0001, 0.0001]]),np.zeros((1,6),float),np.array([[0.0001, 0.0001, 0.0001]]),np.zeros((1,3),float)),axis=1)
+        self.Qk = np.diag(arr)
+        self.calib_result = np.array([0,0,0],float)
         self.ini_ori = np.identity(3)
-        self.orientation_predicted = self.ini_ori
-        self.orientation_corrected = self.ini_ori
-        self.x_states = np.array([0, 0, 0, 0, 0, 0], float).transpose()
-        self.x_states_predicted = self.x_states
-        self.error_states_predicted = np.zeros((15,1),float).transpose()
-        self.error_states = np.zeros((15, 1), float).transpose()
-        self.U = np.array([0, 0, 0,0,0,0], float).transpose()
+        self.orientation = self.ini_ori
+        self.x_states = np.array([0, 0, 0, 0, 0, 0], float)
+        self.error_states= np.zeros((15,1),float)
+        self.U = np.array([[0, 0, 0,0,0,0]], float).T
         self.time_t = 0.0
         self.DT = 0.0
         self.calibrate_count = 0
         self.quat_gy = Quaternion(array=[0,0,0,0])
-        self.acc_bias = np.array([1.5,1.5,1.5])
-        self.gyro_bias = np.array([1.5,1.5,1.5])
+        self.acc_bias = np.array([[0.21,0.06,0.1]]).T
+        self.gyro_bias = np.array([[1.5,1.5,1.5]]).T
+        self.z = np.zeros([1,7],float)
         self.first_init()
 
     def first_init(self):
@@ -107,81 +97,69 @@ class main_Class(object):
 
     def motion_model(self, U):
         T = self.DT
-        """self.A = np.array([[1, 0, 0, T, 0, 0],
-                      [0, 1, 0, 0, T, 0],
-                      [0, 0, 1, 0, 0, T],
-                      [0, 0, 0, 1, 0, 0],
-                      [0, 0, 0, 0, 1, 0],
-                      [0, 0, 0, 0, 0, 1]], float)
-
-        self.B = np.array([[((T ** 2) / 2), 0, 0],
-                      [0, ((T ** 2) / 2), 0],
-                      [0, 0, ((T ** 2) / 2)],
-                      [T, 0, 0],
-                      [0, T, 0],
-                      [0, 0, T]], float)
-
-        rotation = np.array([[1, 0, 0, 0, 0, 0],
-                             [0, 1, 0, 0, 0, 0],
-                             [0, 0, 1, 0, 0, 0],
-                             [0, 0, 0, sensr.Rotation[0][0], sensr.Rotation[0][1], sensr.Rotation[0][2]],
-                             [0, 0, 0, sensr.Rotation[1][0], sensr.Rotation[1][1], sensr.Rotation[1][2]],
-                             [0, 0, 0, sensr.Rotation[2][0], sensr.Rotation[2][1], sensr.Rotation[2][2]]], float)
-        U_vec = (1-0.2)*self.U+0.2*U
-        #U_vec = 0.5 * (self.U + U)
-        self.U = U
-        orientation = sensr.Orientation
-        orientation1 = sensr.Orientation_1
-        self.x_states = np.matmul(self.A, np.matmul(rotation, self.x_states)) + np.matmul(self.B, np.matmul(orientation, U_vec))
-        self.x_states_1 = np.matmul(self.A, np.matmul(rotation, self.x_states_1)) + np.matmul(self.B, np.matmul(orientation1, U_vec))
-        #print('states: ', self.x_states)
-        #print('states2: ', self.x_states_1)"""
-
-
         dOmega = np.array([
             [0, -U[2], U[1]],
             [U[2], 0, -U[0]],
             [-U[1], U[0], 0]
         ])
-        self.orientation_predicted = np.matmul(self.orientation_corrected, np.matmul((2 * np.identity(3, float) + dOmega*T), np.linalg.inv((2 * np.identity(3, float) - dOmega*T))))
-        self.x_states_predicted[0:2,0] = self.x_states[0:2,0]  + T * (self.x_states[3:5,0]) + 0.5*T**2*(self.orientation_predicted*(U[3:5]) - np.array([0.0,0.0,9.8]).transpose())
-        self.x_states_predicted[3:5,0] = self.x_states[3:5,0] + T*(self.orientation_predicted*(U[3:5]) - np.array([0.0,0.0,9.8]).transpose())
+        
+        self.orientation = (self.orientation.dot((2*np.identity(3) + dOmega*T).dot(np.linalg.inv((2 * np.identity(3, float) - dOmega*T)))))
+        self.x_states[0:3] = self.x_states[0:3]  + (T * (self.x_states[3:6]) + (0.5*T**2)*(self.orientation.dot(U[3:6])) - np.array([0.0,0.0,9.8]))
+        self.x_states[3:6] = self.x_states[3:6] + (T*(self.orientation.dot(U[3:6])) - np.array([0.0,0.0,9.8]))
         #print('x_states: ',self.x_states_predicted)
         #S = Quaternion(scalar=0.0, vector=[U[3],U[4],U[5]])
         #qdot = (0.5 * (self.quat_gy * S))
         #quat = self.quat_gy +  (qdot * T)
         #self.quat_gy = quat.normalised
-        error_states = self.error_motion_model(self.orientation_predicted,U)
-        #print('error:', error_states)
-        return error_states
+        #error_states = self.error_motion_model(self.orientation,U)
+        #print(self.x_states)
+        
 
-    def error_motion_model(self,orientation,U):
-        accn = np.matmul(orientation,U[0,3:5])
+    def error_motion_model(self,U):
+        accn = (self.orientation.dot(U[3:6]))
         S = np.array([[0,-accn[2],accn[1]],
                       [accn[2],0,-accn[0]],
                       [-accn[1],accn[0],0]])      #The acceleration is bias corrected and transformed to the navigation frame accn = Rot_b_to_n * accn_body
+		
+        phi1_1 = np.concatenate((np.identity(3),self.DT*self.orientation),axis=1)
+        phi1 = np.concatenate((phi1_1,np.zeros([3,9],float)),axis=1)
+        
+        phi2_1 = np.concatenate((np.zeros([3,3],float),np.identity(3)),axis=1)
+        phi2 = np.concatenate((phi2_1,np.zeros([3,9],float)),axis=1)
+        
+        phi3_1 = np.concatenate((np.zeros([3,3],float),np.zeros([3,3],float)),axis=1)
+        phi3_2 = np.concatenate((phi3_1,np.identity(3)),axis=1)
+        phi3_3 = np.concatenate((self.DT*np.identity(3),np.zeros([3,3],float)),axis=1)
+        phi3 = np.concatenate((phi3_2,phi3_3),axis=1)
+        
+        phi4_1 = np.concatenate((-self.DT*S, np.zeros([3,6],float)),axis=1)
+        phi4_2 = np.concatenate((phi4_1, np.identity(3)),axis=1)
+        phi4 = np.concatenate((phi4_2,self.DT*self.orientation),axis=1)
+        
+        phi5 = np.concatenate((np.zeros([3,12],float),np.identity(3)),axis=1)
+        
+        
+        self.phi = np.concatenate((phi1,phi2,phi3,phi4,phi5),axis=0)
+        self.error_states = self.Phi.dot(self.error_states)
+        
+        return self.error_states
 
-        self.Phi = np.array([[np.identity(3,float), self.DT*orientation,np.zeros((3,9),float)],
-                       [np.zeros((3,3),float),np.identity(3,float),np.zeros((3,9),float)],
-                       [np.zeros((3,3),float),np.zeros((3,3),float),np.identity(3,float),self.DT*np.identity(3,float),np.zeros((3,3),float)],
-                       [-self.DT*S, np.zeros((3,6),float),np.identity(3), self.DT*orientation],
-                        [np.zeros((3,12),float),np.identity(3,float)]  ])
-        error_states = np.matmul(self.Phi,self.error_states)
-        """dPhi = self.error_states[0:2]
-        dTheta = np.array([
-            [0, dPhi[2], -dPhi[1]],
-            [-dPhi[2], 0, dPhi[0]],
-            [dPhi[1], -dPhi[0], 0]
-        ], float)
-        self.orientation = np.matmul(np.matmul((2 * np.identity(3, float) + dTheta), (2 * np.identity(3, float) - dTheta)), self.orientation)"""
-        return error_states
-
-    def error_observation_model(self,states):
-        self.H = np.array([[0, 0, 1, np.zeros((1,12),float)],
+    def error_observation_model(self):
+        h1 = np.concatenate((np.array([[0,0,1]],float),np.zeros([1,12],float)),axis=1)
+        h2 = np.concatenate((np.zeros((3,3),float),np.identity(3,float),np.zeros((3,9),float)),axis=1)
+        h3 = np.concatenate((np.zeros((3,9),float), np.identity(3,float),np.zeros((3,3),float)),axis=1)
+        """self.H = np.array([[0, 0, 1, np.zeros((1,12),float)],
                       [np.zeros((3,3),float),np.identity(3,float),np.zeros((3,9),float)],
-                      [np.zeros((3,9),float), np.identity(3,float),np.zeros((3,3),float)]])
-        Z = np.matmul(self.H,states)
+                      [np.zeros((3,9),float), np.identity(3,float),np.zeros((3,3),float)]])"""
+        self.H = np.concatenate((h1,h2,h3),axis=0)
+        Z = self.H.dot(self.error_states)
         return Z
+    def get_heading(self):
+        quat = Quaternion(matrix = self.orientation)
+        q = quat.elements
+        yaw = (math.atan2(2.0 * (q[1] *q[2] - q[0] * q[3]),
+                                                       -1+2*(q[0] * q[0] + q[1] * q[1])))
+        return yaw
 
     def kalman_filter(self, U_vec, zk=None, location=None, flag=False):
         """Performs Kalman Filtering on pandas timeseries data.
@@ -198,58 +176,45 @@ class main_Class(object):
         :return: output (np.array): kalman filtered data
         """
         try:
-            """
-            output = np.zeros(len(zk))
-            H = np.array([[1,0,0,0,0,0],[0,1,0,0,0,0],[0,0,1,0,0,0]])
-            # time update (prediction)
-            self.x_states = self.motion_model(U_vec,sensr)
-            #self.x_states = np.matmul(self.A,self.x_states) + np.matmul(self.B,self.U) #Predict state
-            zk_pred = np.matmul(H,self.x_states) # Predict measurement
-            self.Pk = np.matmul(self.A,np.matmul(self.Pk,self.A.T)) + self.Q # Predict error covariance
-            # measurement update (correction)
-            vk = zk - zk_pred # Measurement residual
-            S = (np.matmul(H,np.matmul(self.Pk,H.T))) + self.R # Prediction covariance
-            Kk = np.matmul(np.matmul(self.Pk,H.transpose()), np.linalg.inv(S)) # Compute Kalman Gain
-            self.x_states = self.x_states + np.matmul(Kk , vk) # Update estimate with gain * residual
-            self.Pk = np.matmul((1 - np.matmul(Kk,H)),self.Pk) # Update error covariance
-            output = self.x_states
-            #print('Output: ', output)
-            return output"""
-            if (flag):          #Not stationary
+           
+            if (not flag):          #Not stationary
                 #Prediction
-                self.error_states = self.motion_model(U_vec)
-                prediction = self.error_observation_model(self.error_states)
+                self.motion_model(U_vec)
+                prediction = self.error_observation_model()
                 self.Pk = np.matmul(self.Phi,np.matmul(self.Pk,self.Phi.T)) + self.Qk
-            else:           #Stationary
+                self.error_states = np.zeros([1,15],float)
+            else:                   #Stationary
                 # Prediction
-                self.error_states = self.motion_model(U_vec)
-                prediction = self.error_observation_model(self.error_states)
+                self.error_states = self.error_motion_model(U_vec)
+                prediction = self.error_observation_model()
                 self.Pk = np.matmul(self.Phi, np.matmul(self.Pk, self.Phi.T)) + self.Qk
-                self.orientation_corrected = self.orientation_predicted
                 #Correction
-                vk = zk - prediction
+                vk = zk.reshape(-1,1) - prediction
+                
                 S = (np.matmul(self.H, np.matmul(self.Pk, self.H.T))) + self.Rk
                 Kk = np.matmul(np.matmul(self.Pk, self.H.T), np.linalg.inv(S))
                 self.error_states = self.error_states + np.matmul(Kk, vk)  # Update estimate with gain * residual
                 self.Pk = np.matmul((np.identity(15) - np.matmul(Kk, self.H)), self.Pk)  # Update error covariance
                 output = self.error_states
-                self.x_states[0:2] = self.x_states_predicted[0:2] - output[6:8]
-                self.x_states[3:5] = self.x_states_predicted[3:5] - output[9:11]
+                #print(self.x_states)
+                self.x_states[0:3] = self.x_states[0:3] - output[6:9].T.flatten()
+                self.x_states[3:6] = self.x_states[3:6] - output[9:12].T.flatten()
 
-                dPhi = self.error_states[0:2]
+                dPhi = self.error_states[0:3]
                 dTheta = np.array([
                     [0, dPhi[2], -dPhi[1]],
                     [-dPhi[2], 0, dPhi[0]],
                     [dPhi[1], -dPhi[0], 0]
                 ], float)
-                self.orientation_corrected = np.matmul(
-                    np.matmul((2 * np.identity(3, float) + dTheta), (2 * np.identity(3, float) - dTheta)),
-                    self.orientation_predicted)
+                self.orientation = np.matmul(
+                    np.matmul((2 * np.identity(3) + dTheta), np.linalg.inv(2*np.identity(3) - dTheta)),
+                    self.orientation)
+            #print("States: ", self.x_states)
         except np.linalg.linalg.LinAlgError:
             pass
 
     def main(self):
-        #sensr = sensor_fusion(self.ini_ori, self.time_t)
+        sensr = sensor_fusion(self.ini_ori, self.time_t)
         #self.quat = sensr.quat
         while 1:
             try:
@@ -263,24 +228,21 @@ class main_Class(object):
                 
                 if ((not (int(data[1]) == 255)) and len(data) == 14):
                     self.time_update(float(data[2]))
-                    magr = np.subtract(np.array([float(data[9]), float(data[10]), float(data[11])]).transpose(),self.calib_result/self.calibrate_count)
-                    accn = np.subtract(np.array([float(data[3]), float(data[4]), float(data[5])]),np.array([self.error_states[0,12],self.error_states[0,13],self.error_states[0,14]]))
-                    gyro = np.subtract(np.array([float(data[6]), float(data[7]), float(data[8])]),np.array([self.error_states[0,3],self.error_states[0,4],self.error_states[0,5]]))
-                    print('U: ', gyro)
-                    U_vec = np.array([gyro[0],gyro[1],gyro[2],accn[0],accn[1],accn[2]])
-                    
-                    self.motion_model(U_vec)
-                    #sensr.set_angles(alpha=float(data[6]), phi=float(data[7]), theta=float(data[8]),acc= accn,mag=magr,time_T=float(data[2]))
-                    #if abs((np.linalg.norm(accn))-9.8) <=1.5:
-                     #   self.kalman_filter()
-                    #U_vec = np.subtract(np.array([float(data[3]), float(data[4]), float(data[5])], float).transpose(),
-                    #                    sensr.gravity)
-                    #xk = self.motion_model(U_vec, sensr)
-                    #zk = np.array([0,0,0]).T
-                    #final_xyz = self.kalman_filter(zk,U_vec,sensr)
-                    #self.filesave(str(final_xyz))
-
-
+                    magr = (np.array([float(data[9]), float(data[10]), float(data[11])]).reshape(-1,1)-(self.calib_result/self.calibrate_count).reshape(-1,1))
+                    accn = (np.array([float(data[3]), float(data[4]), float(data[5])]).reshape(-1,1)-self.acc_bias-np.array([self.error_states[12][0],self.error_states[13,0],self.error_states[14,0]]).reshape(-1,1))
+                    gyro = (np.array([float(data[6]), float(data[7]), float(data[8])]).reshape(-1,1)-self.gyro_bias-np.array([self.error_states[3],self.error_states[4,0],self.error_states[5,0]]).reshape(-1,1))
+                    U_vec = np.concatenate((gyro.T,accn.T),axis=1).flatten()
+                    sensr.set_angles(accn,magr,self.DT)
+                    yaw = self.get_heading()
+                    print(np.array([float(data[6]), float(data[7]), float(data[8])]))
+                    if(abs(11-abs(np.linalg.norm(accn)))<=2):
+                        self.motion_model(U_vec)
+                        self.z[0,2] = sensr.yaw_a - yaw
+                        self.z[0,3:6] = gyro.flatten()
+                        self.kalman_filter(U_vec=U_vec,zk=self.z,location=None,flag=True)
+                    else:
+                        self.kalman_filter(U_vec=U_vec,zk=None,location=None,flag=False)
+  
             finally:
                 pass
 
